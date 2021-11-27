03c74a08454e4b420d20000c2d077d067385b11d
Kurtis Doute <kcd257@yahoo.com>
2021-11-27 12:40:46 -0800

    Timer is now functional and appearing on the screen and displaying an alert when time has run out

--- Assets/css/js/script.js
+++ Assets/css/js/script.js
@@ -1,16 +1,48 @@
-function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
-    function showQuestion(questions, quizContainer) {
+var timerEl = document.querySelector(".timer");
+var startBtn = document.getElementById("startBtn");
 
-    }
+console.log("hello");
 
-    function showResults(questions, quizContainer, resultsContainer) {
+startBtn.addEventListener("click", startGame);
 
-    }
+function startGame() {
+    var timeLeft = 16;
+  
+    var timeInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = timeLeft + " seconds remaining";
+        console.log(timerEl);
+      if(timeLeft === 0) {
+        clearInterval(timeInterval);
+        alert("Time's Up!");
+      }
+      console.log(timeLeft);
+    }, 1000);
+  }
 
-    showQuestions(qustions, quizContainer);
+// var questions = [{
+//     questions: "What is the name of the main character in the Kingdom Hearts franchise?",
+//     answers: ["Goofy", "Kairi", "Sora", "Riku"],
+//     correctAnswer: "Sora"
+// },{
+//     questions: "Who is the main antagonist of Birth By Sleep?",
+//     answers: ["Xehanort", "Donald", "Terra", "Ventus"],
+//     correctAnswer: "Xehanort"
+// }]
 
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
+// function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
+//     function showQuestion(questions, quizContainer) {
 
-}
+//     }
+
+//     function showResults(questions, quizContainer, resultsContainer) {
+
+//     }
+
+//     showQuestions(qustions, quizContainer);
+
+//     submitButton.onclick = function(){
+//         showResults(questions, quizContainer, resultsContainer);
+//     }
+
+// }

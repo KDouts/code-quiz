4b84f53776fe2cb6a730c71bd328062f869d413a
Kurtis Doute <kcd257@yahoo.com>
2021-11-27 14:55:56 -0800

    Updating js file

--- Assets/css/js/script.js
+++ Assets/css/js/script.js
@@ -18,31 +18,31 @@ function startGame() {
       }
       console.log(timeLeft);
     }, 1000);
+    
+    // var questions = [{
+    //     questions: "What is the name of the main character in the Kingdom Hearts franchise?",
+    //     answers: ["Goofy", "Kairi", "Sora", "Riku"],
+    //     correctAnswer: "Sora"
+    // },{
+    //     questions: "Who is the main antagonist of Birth By Sleep?",
+    //     answers: ["Xehanort", "Donald", "Terra", "Ventus"],
+    //     correctAnswer: "Xehanort"
+    // }]
+    
+    // function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
+    //     function showQuestion(questions, quizContainer) {
+    
+    //     }
+    
+    //     function showResults(questions, quizContainer, resultsContainer) {
+    
+    //     }
+    
+    //     showQuestions(qustions, quizContainer);
+    
+    //     submitButton.onclick = function(){
+    //         showResults(questions, quizContainer, resultsContainer);
+    //     }
+    
+    // }
   }
-
-// var questions = [{
-//     questions: "What is the name of the main character in the Kingdom Hearts franchise?",
-//     answers: ["Goofy", "Kairi", "Sora", "Riku"],
-//     correctAnswer: "Sora"
-// },{
-//     questions: "Who is the main antagonist of Birth By Sleep?",
-//     answers: ["Xehanort", "Donald", "Terra", "Ventus"],
-//     correctAnswer: "Xehanort"
-// }]
-
-// function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
-//     function showQuestion(questions, quizContainer) {
-
-//     }
-
-//     function showResults(questions, quizContainer, resultsContainer) {
-
-//     }
-
-//     showQuestions(qustions, quizContainer);
-
-//     submitButton.onclick = function(){
-//         showResults(questions, quizContainer, resultsContainer);
-//     }
-
-// }

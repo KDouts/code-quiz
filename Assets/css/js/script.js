4c5e0132a0dbb49f3f20ed346b95fb96e79ece36
Kurtis Doute <kcd257@yahoo.com>
2021-11-28 03:12:04 -0800

    When the start button is pressed the question and answer buttons show on the page

--- Assets/css/js/script.js
+++ Assets/css/js/script.js
@@ -1,48 +1,68 @@
 var timerEl = document.querySelector(".timer");
 var startBtn = document.getElementById("startBtn");
+var questionEl = document.getElementById("#quiz");
+var question, chA, chB, chC, chD;
+var i = 0;
+var score = 0;
 
 console.log("hello");
+
+var myQuestions = [{
+   question: "Question #1: What is the name of the main character in the Kingdom Hearts franchise?",
+   answers: { 
+   a: "Goofy", 
+   b: "Kairi", 
+   c: "Sora", 
+   d: "Riku"},
+   correctAnswer: "c"
+},{
+   question: "Question #2: Who is the main antagonist of Birth By Sleep?",
+   answers: {
+     a: "Xehanort", 
+     b: "Donald", 
+     c: "Terra", 
+     d: "Ventus"},
+     correctAnswer: "d"
+   }];
+
+   console.log(myQuestions);
 
 startBtn.addEventListener("click", startGame);
 
 function startGame() {
-    var timeLeft = 16;
-  
-    var timeInterval = setInterval(function () {
-      timeLeft--;
-      timerEl.textContent = timeLeft + " seconds remaining";
-        console.log(timerEl);
-      if(timeLeft === 0) {
-        clearInterval(timeInterval);
-        alert("Time's Up!");
-      }
-      console.log(timeLeft);
-    }, 1000);
-    
-    // var questions = [{
-    //     questions: "What is the name of the main character in the Kingdom Hearts franchise?",
-    //     answers: ["Goofy", "Kairi", "Sora", "Riku"],
-    //     correctAnswer: "Sora"
-    // },{
-    //     questions: "Who is the main antagonist of Birth By Sleep?",
-    //     answers: ["Xehanort", "Donald", "Terra", "Ventus"],
-    //     correctAnswer: "Xehanort"
-    // }]
-    
-    // function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
-    //     function showQuestion(questions, quizContainer) {
-    
-    //     }
-    
-    //     function showResults(questions, quizContainer, resultsContainer) {
-    
-    //     }
-    
-    //     showQuestions(qustions, quizContainer);
-    
-    //     submitButton.onclick = function(){
-    //         showResults(questions, quizContainer, resultsContainer);
-    //     }
-    
-    // }
+
+
+  var timeLeft = 11;
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft + " seconds remaining";
+      console.log(timerEl);
+    if(timeLeft === 0) {
+      clearInterval(timeInterval);
+      alert("Time's Up!");
+    }
+    console.log(timeLeft);
+  }, 1000);
+
+  for(i=0; i < myQuestions.length; i++){
+    var answer = myQuestions[i];
+    if(answer == myQuestions[i].correctAnswer) {
+      score++;
+    }
   }
+
+  var questions = myQuestions[0].question;
+  var chA = myQuestions[0].answers.a;
+  var chB = myQuestions[0].answers.b;
+  var chC = myQuestions[0].answers.c;
+  var chD = myQuestions[0].answers.d;
+
+  quiz.innerHTML = "<h3>" +questions+ "</h3>"
+
+  quiz.innerHTML += "<button>" +chA+ "</button>"
+  quiz.innerHTML += "<button>" +chB+ "</button>"
+  quiz.innerHTML += "<button>" +chC+ "</button>"
+  quiz.innerHTML += "<button>" +chD+ "</button>"
+
+  console.log(score);
+}

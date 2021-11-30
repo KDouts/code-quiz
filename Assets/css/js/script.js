e325ab113bc88557db20daad6d469ff2a4ab665c
Kurtis Doute <kcd257@yahoo.com>
2021-11-29 18:21:43 -0800

    Made code easier to read

--- Assets/css/js/script.js
+++ Assets/css/js/script.js
@@ -1,6 +1,7 @@
 var timerEl = document.querySelector(".timer");
 var startBtn = document.getElementById("startBtn");
 var answerEl = document.getElementsByClassName(".answerBtn");
+var timeLeft = 151;
 var question
 var currentQuestion = 0;
 var score = 0;
@@ -20,76 +21,74 @@ var myQuestions = [{
       b: "Donald", 
       c: "Terra", 
       d: "Ventus"},
-      correctAnswer: 3
+      correctAnswer: 1
     },
     {question: "Question #3: Who is the most powerful character?",
     answers: {
-      a: "Xehanort", 
+      a: "Aqua", 
       b: "Donald", 
       c: "Terra", 
       d: "Ventus"},
       correctAnswer: 1
-    }];
+}];
     
-    console.log(myQuestions);
-    
-    startBtn.addEventListener("click", startGame);
-    
-    function startGame() {
-      
-      updateUI();
-      getAnswer();
-      startTimer();
-      
-    function startTimer() {
-      var timeLeft = 11;
-      var timeInterval = setInterval(function () {
-        timeLeft--;
-        timerEl.textContent = timeLeft + " seconds remaining";
-      // console.log(timerEl);
-      if(timeLeft === 0) {
-        clearInterval(timeInterval);
-        alert("Time's Up!");
-      }
-      // console.log(timeLeft);
-    }, 1000);
+console.log(myQuestions);
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+  
+  updateUI();
+  getAnswer();
+  startTimer();
+
+}
+function getAnswer() {
+  let btn = document.querySelectorAll(".answerBtn");
+  for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", bindClick(i));
+  }
+  
+}
+function bindClick(i) {
+  return function () {
+    console.log("you clicked region number " + i);
+    console.log(myQuestions[currentQuestion].correctAnswer)
+    if(i === myQuestions[currentQuestion].correctAnswer) {
+      score++;
+      console.log("correct!");
+    } else if (i != myQuestions[currentQuestion].correctAnswer) {
+      timeLeft-10;
     }
-    
-    
-    function updateUI() {
-      var questions = myQuestions[currentQuestion].question;
-        var chA = myQuestions[currentQuestion].answers.a;
-        var chB = myQuestions[currentQuestion].answers.b;
-        var chC = myQuestions[currentQuestion].answers.c;
-        var chD = myQuestions[currentQuestion].answers.d;
-          
-        quiz.innerHTML = "<h3>" +questions+ "</h3>"
-          
-        quiz.innerHTML += "<button Class='answerBtn'> A: " +chA+ "</button>"
-        quiz.innerHTML += "<button Class='answerBtn'> B: " +chB+ "</button>"
-        quiz.innerHTML += "<button Class='answerBtn'> C: " +chC+ "</button>"
-        quiz.innerHTML += "<button Class='answerBtn'> D: " +chD+ "</button>"
-    }  
+    currentQuestion++;
+    updateUI();
+    getAnswer();
+  };
+}
+function updateUI() {
+var questions = myQuestions[currentQuestion].question;
+var chA = myQuestions[currentQuestion].answers.a;
+var chB = myQuestions[currentQuestion].answers.b;
+var chC = myQuestions[currentQuestion].answers.c;
+var chD = myQuestions[currentQuestion].answers.d;
 
-      function getAnswer() {
-        let btn = document.querySelectorAll(".answerBtn");
-        for (var i = 0; i < btn.length; i++) {
-            btn[i].addEventListener("click", bindClick(i));
-        }
+quiz.innerHTML = "<h3>" +questions+ "</h3>"
 
-        function bindClick(i) {
-            return function () {
-                console.log("you clicked region number " + i);
-                console.log(myQuestions[currentQuestion].correctAnswer)
-                if(i === myQuestions[currentQuestion].correctAnswer) {
-                  score++;
-                }
-                currentQuestion++;
-                updateUI();
-                getAnswer();
-                startTimer();
+quiz.innerHTML += "<button Class='answerBtn'> A: " +chA+ "</button>"
+quiz.innerHTML += "<button Class='answerBtn'> B: " +chB+ "</button>"
+quiz.innerHTML += "<button Class='answerBtn'> C: " +chC+ "</button>"
+quiz.innerHTML += "<button Class='answerBtn'> D: " +chD+ "</button>"
+}
 
-            };
-        }
-    }
- }
+function startTimer() {
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft + " seconds remaining";
+  // console.log(timerEl);
+  if(timeLeft === 0) {
+    clearInterval(timeInterval);
+    alert("Time's Up!");
+  }
+  // console.log(timeLeft);
+}, 1000);
+}
